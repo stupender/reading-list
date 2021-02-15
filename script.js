bfb3d4b548ad74ecfd1c98b2fef683c7636f46b4
Stu Pender <stu1992@gmail.com>
2021-02-15 17:50:06 -0600

    html/css

---
+++ script.js
@@ -0,0 +1,31 @@
+let library = [];
+
+function Book() {
+    // the constructor...loops through the array to display each book on the page
+}
+
+function addBookToLibrary() {
+    // do stuff here...
+}
+
+
+
+
+
+
+
+
+
+// 1. Write a function that loops through the array and displays each book on the page. 
+// You can display them in some sort of table, or each on their own “card”. 
+// It might help for now to manually add a few books to your array so you can see the display.
+
+// 2. Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: 
+// author, title, number of pages, whether it’s been read and anything else you might want.
+
+// 3. Add a button on each book’s display to remove the book from the library.
+// 3a. You will need to associate your DOM elements with the actual book objects in some way. 
+// One easy solution is giving them a data-attribute that corresponds to the index of the library array.
+
+// 4. Add a button on each book’s display to change its read status.
+// 4a. To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
